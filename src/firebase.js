9e5e83eafeca4d65a6fe45a49e8f949e76a8f58a
FernandoAltamirano <fernando.altamirano@unmsm.edu.pe>
2021-09-24 13:27:13 -0500

    Modified also things in the funtionality

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,7 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBwnHuJ-24qp9TUG1T6fsQy5zV2KdnQ-dE",
-  authDomain: "ecommerce-guantes.firebaseapp.com",
-  projectId: "ecommerce-guantes",
-  storageBucket: "ecommerce-guantes.appspot.com",
-  messagingSenderId: "539701802922",
-  appId: "1:539701802922:web:40fc9ecac4ede88b9e5dd3",
-  measurementId: "G-GJVYESLBCM",
-};
+import { firebaseConfig } from "../config";
 
 firebase.apps.length < 1 && firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
